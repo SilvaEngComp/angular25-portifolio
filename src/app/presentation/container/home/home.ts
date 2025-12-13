071efeea79cdea927f60b83474f11b5e28e41d74
M538308 <eliabe.nc.silva@bradesco.com.br>
2025-12-13 08:22:03 -0300

    customizing main section

--- src/app/presentation/container/home/home.ts
+++ src/app/presentation/container/home/home.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { BtnPrimary } from "../../component/btn-primary/btn-primary";
-import { Header } from "../../component/header/header";
-import { MainSection } from "../../component/main-section/main-section";
+import { Header } from '../../component/header/header';
+import { MainSection } from '../../component/main-section/main-section';
 
 @Component({
   selector: 'app-home',
-  imports: [BtnPrimary, Header, MainSection],
+  imports: [Header, MainSection],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {
-
-}
+export class Home {}
