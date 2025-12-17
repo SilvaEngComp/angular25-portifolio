3283f9eb2e0f443c7d99410cec8451268fab2f2d
M538308 <eliabe.nc.silva@bradesco.com.br>
2025-12-17 09:01:09 -0300

    resizing main-animation

--- src/app/presentation/component/main-section/main-section.ts
+++ src/app/presentation/component/main-section/main-section.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { MainAnimation } from "../main-animation/main-animation";
+import { MainAnimation } from '../main-animation/main-animation';
+import { BtnPrimary } from '../btn-primary/btn-primary';
 
 @Component({
   selector: 'main-section',
-  imports: [MainAnimation],
+  imports: [MainAnimation, BtnPrimary],
   templateUrl: './main-section.html',
   styleUrl: './main-section.scss',
 })
-export class MainSection {
-
-}
+export class MainSection {}
